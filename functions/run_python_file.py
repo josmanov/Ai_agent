5824673dd62bc866edd6a1b3125703c154c81117
josmanov <jusuf.osmanovic01@gmail.com>
2026-01-10 13:41:52 +0200

    create python path running capability with subprocess

---
+++ functions/run_python_file.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path, args=None):
+            
+    working_dir_abs = os.path.abspath("calculator")
+    target_dir = os.path.normpath(os.path.join(working_dir_abs, file_path))
+    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+
+    if valid_target_dir == False:
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if os.path.isfile(target_dir) == False:
+        return f'Error: "{file_path}" does not exist or is not a regular file'
+    if file_path.endswith('.py') == False:
+        return f'Error: "{file_path}" is not a Python file'
+
+    command = ["python", target_dir]
+    if not args:
+        pass
+    else:
+        command.extend(args)
+    try:
+        output_parts = []
+        result = subprocess.run(command, cwd=working_dir_abs , capture_output=True, text=True, timeout=30)
+        if not result.stdout and not result.stderr:
+            output_parts.append("No output produced")
+        else:
+            if result.returncode != 0:
+                output_parts.append(f"Process exited with code {result.returncode}")
+            if result.stdout:
+                output_parts.append(f"STDOUT:\n{result.stdout}")
+            if result.stderr:
+                output_parts.append(f"STDERR:\n{result.stderr}")
+        output = "\n".join(output_parts)
+        return output
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
